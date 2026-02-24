6e29af4074f1e80bfd4c8bf32023c0e39fcc1b35
haiseno <haise.zhao@gmail.com>
2026-02-23 16:31:33 -0800

    Refactor to Express server

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+const options = {
+  root: path.join(__dirname, "views"),
+};
+
+app.get(["/", "/home"], (req, res) => res.sendFile("index.html", options));
+app.get("/about", (req, res) => res.sendFile("about.html", options));
+app.get("/contact", (req, res) => res.sendFile("contact.html", options));
+app.use((req, res) => res.sendFile("404.html", options));
+
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:3000");
+});
